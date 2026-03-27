754a1b209e650cf250741c0661f0b62366482d4e
HeYang <heyangev@gmail.com>
2026-03-27 14:30:42 +0800

    refactor: collapse audit repository bridges

--- internal/repository/audit_log.go
+++ internal/repository/audit_log.go
@@ -5,9 +5,44 @@ import (
 	"gorm.io/gorm"
 )
 
+const (
+	RiskLevelLow      = auditrepo.RiskLevelLow
+	RiskLevelMedium   = auditrepo.RiskLevelMedium
+	RiskLevelHigh     = auditrepo.RiskLevelHigh
+	RiskLevelCritical = auditrepo.RiskLevelCritical
+)
+
 type AuditLogListOptions = auditrepo.AuditLogListOptions
 type AuditLogRepository = auditrepo.AuditLogRepository
+type AuditStats = auditrepo.AuditStats
+type ActionStat = auditrepo.ActionStat
+type UserRanking = auditrepo.UserRanking
+type ActionGroupItem = auditrepo.ActionGroupItem
+type ResourceTypeGroupItem = auditrepo.ResourceTypeGroupItem
+type TrendItem = auditrepo.TrendItem
+type HighRiskLog = auditrepo.HighRiskLog
+type RiskRule = auditrepo.RiskRule
+type HighRiskRule = auditrepo.HighRiskRule
+
+var RiskRules = auditrepo.RiskRules
+var HighRiskRules = auditrepo.HighRiskRules
 
 func NewAuditLogRepository(db *gorm.DB) *AuditLogRepository {
 	return auditrepo.NewAuditLogRepository(db)
+}
+
+func IsHighRisk(action, resourceType string) bool {
+	return auditrepo.IsHighRisk(action, resourceType)
+}
+
+func GetRiskLevel(action, resourceType string) string {
+	return auditrepo.GetRiskLevel(action, resourceType)
+}
+
+func GetRiskReason(action, resourceType string) string {
+	return auditrepo.GetRiskReason(action, resourceType)
+}
+
+func buildHighRiskCondition() string {
+	return auditrepo.BuildHighRiskCondition()
 }
